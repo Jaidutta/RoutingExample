b0b8e4ef6f990ab2245a581dce0e8c675e4e3fa0
Joy Dutta <dutta_joy@hotmail.com>
2023-12-22 01:22:32 +0000

    4/02. Map, MapGet, MapPost

--- RoutingExample/Program.cs
+++ RoutingExample/Program.cs
@@ -8,6 +8,16 @@ app.UseRouting();
 app.UseEndpoints(endpoints =>
 {
     //add your endpoints here
+    endpoints.MapGet("map1", async (context) => {
+        await context.Response.WriteAsync("In Map 1");
+    });
 
+    endpoints.MapPost("map2", async (context) => {
+        await context.Response.WriteAsync("In Map 2");
+    });
+});
+
+app.Run(async context => {
+    await context.Response.WriteAsync($"Request received at {context.Request.Path}");
 });
 app.Run();
